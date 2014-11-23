76549e48ce88542f452f7dc3f35871ed7ad9bc6a
Wesley A. Pettyjohn <w.a.pettyjohn@gmail.com>
2014-11-23 10:35:46 +0000

    resolving merge conflicts

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -117,7 +117,6 @@ function scaleTagCloud() {
     maxWidth = $jobTags = $jobTagsWidth = null;
 }
 
-<<<<<<< HEAD
 /* .view-all handling */ 
 jQuery( '.view-all' ).click(
     function( event ) {
@@ -135,15 +134,12 @@ jQuery( '.view-all' ).click(
     }
 );
 
-=======
->>>>>>> 77248ddeca2a06bcb9bfad12e8cf263b7d7c3854
 
 /* Tag cloud filter */
 function initTagCloudFilter( initialColor, selectedColor ) {
     jQuery( '.skill > a' ).click(
 	function( event ) {
 	    event.preventDefault();
-<<<<<<< HEAD
 
 	    var $clickedSkill = jQuery( this );
 	    var clickedSkillText = jQuery.trim( $clickedSkill.text() );
@@ -158,21 +154,14 @@ function initTagCloudFilter( initialColor, selectedColor ) {
 	    jQuery( '.arrow' ).html( '&#9662;' );
 	    jQuery( '.skill > a' ).css( 'color', initialColor );
 	    $clickedSkill.css( 'color', selectedColor );
-=======
-	    
-	    var $clickedTag = jQuery( this );
-	    var clickedTagText = jQuery.trim( $clickedTag.text() );
-	    var $jobs = jQuery( '.job' );
 
-	    jQuery( '.skill > a' ).css( 'color', initialColor );
-	    $clickedTag.css( 'color', selectedColor );
->>>>>>> 77248ddeca2a06bcb9bfad12e8cf263b7d7c3854
+	    var $jobs = jQuery( '.job' );
 	    $jobs.show();
 
 	    $jobs.each(
 		function() {
 		    var $job = jQuery( this );
-<<<<<<< HEAD
+
 		    var skills = [];
 
 		    $job.find( '.skill' ).each(
@@ -215,36 +204,6 @@ function initTagCloudFilter( initialColor, selectedColor ) {
 		    $job = skills = null;
 		}
 	    );
-
-	    $clickedTag = $viewAll = $jobs = null;
-=======
-		    var tags = [];
-
-		    $job.find( '.skill' ).each(
-			function() {
-			    var $tagAnchor = jQuery( this ).find( '> a' );
-			    var tagText = jQuery.trim( $tagAnchor.text() );
-			    
-			    if ( tagText === clickedTagText ) {
-				$tagAnchor.css( 'color', selectedColor );
-			    }
-			    
-			    tags.push( tagText );
-			    
-			    $tagAnchor = tagText = null;
-			}
-		    );
-		    
-		    if ( tags.indexOf( clickedTagText ) === -1 ) {
-			$job.hide();
-		    }
-
-		    $job = tags = null;
-		}
-	    );
-	    
-	    $clickedTag = clickedTagText = $jobs = null;
->>>>>>> 77248ddeca2a06bcb9bfad12e8cf263b7d7c3854
 	}
     );
 }
@@ -263,7 +222,6 @@ jQuery( document ).ready( function( $ ) {
      * You can remove this if you don't need it
      */
     loadGravatars();
-<<<<<<< HEAD
 
     jQuery( window ).scroll( 
 	function() {
@@ -282,8 +240,6 @@ jQuery( document ).ready( function( $ ) {
 	    $sidebar = null;
 	}
     );
-=======
->>>>>>> 77248ddeca2a06bcb9bfad12e8cf263b7d7c3854
     
     /* Initialize tag cloud */
     jQuery( '#sidebar .skill > a' ).each(
@@ -292,10 +248,7 @@ jQuery( document ).ready( function( $ ) {
 
 	    $tagAnchor.css( 'font-size', $tagAnchor.attr( 'count' ) + 'em' );
 
-<<<<<<< HEAD
 	    // setting min font-size 
-=======
->>>>>>> 77248ddeca2a06bcb9bfad12e8cf263b7d7c3854
 	    if ( parseInt( $tagAnchor.css( 'font-size' ) ) < 16 ) {
 		$tagAnchor.css( 'font-size', '16px'  );
 	    }
@@ -340,11 +293,7 @@ jQuery( document ).ready( function( $ ) {
 
 	    $projectName = $arrow = $projectContent = null;
 	}
-<<<<<<< HEAD
-    );	
-=======
     );		
->>>>>>> 77248ddeca2a06bcb9bfad12e8cf263b7d7c3854
 } ); /* end of as page load scripts */
 
 jQuery( window ).load(
